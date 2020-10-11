e108016f664f60b19826cce6440c0b34dc47b89d
Sara Cemal <saracemal@gmail.com>
2020-10-11 03:02:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,5 @@
 
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
@@ -11,5 +10,6 @@ def toadstool
 end
 
 def link 
+  puts ''
   
   
